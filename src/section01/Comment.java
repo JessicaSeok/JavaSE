212f10a6cfb302319b575dc81e5e3e9c65ee6c72
JessicaSeok <jseok@uw.edu>
2023-07-14 20:19:58 +0900

    첫커

---
+++ src/section01/Comment.java
@@ -0,0 +1,20 @@
+package section01;
+
+/*
+ * 주석 
+ * 	코드에 영향을 주지 않는 개발작 사용하는 메모공간
+ * 줄 삭제: ctrl + d
+ * 
+ */
+public class Comment {
+	public static void main(String[] args) {
+		//한줄 주
+		System.out.println("Hello Java!");
+		
+		printNum(712);
+	}
+	
+	public static void printNum(int num) {
+		System.out.println("num:" + num);
+	}
+} 
